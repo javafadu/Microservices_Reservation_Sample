cc9e36bb43fff6acd4a2ea1874697f27cd4da242
javafadu <javafadu@gmail.com>
2022-08-11 19:44:19 +0300

    11aug2022

---
+++ Microservices/car-service/src/main/java/com/tpe/CarServiceApplication.java
@@ -0,0 +1,15 @@
+package com.tpe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class CarServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarServiceApplication.class, args);
+    }
+
+}
